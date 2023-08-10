ff524f1f2a0ec3d3e1002ef59b38e0cd15616db5
Shweta Rathore <shwetarathore89>
2023-08-10 20:45:30 +0530

    removing unwanted files

--- clients/src/component/Main.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Main() {
-  return (
-      <div className="container-fluid bg-dark text-white d-flex justify-content-center align-items-center" >
-        <h5 style={{fontSize: "50px"}}>NewsHub</h5>  
-    </div>
-  )
-}
